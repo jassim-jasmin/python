f54d1f6c36657c592f134365ea1b981e4099cf5c
jassim <mohammedjassim.jasmin@gmail.com>
2018-12-23 20:22:38 +0530

    Regular Expression

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
+import re
+
+reObj = re.compile(r'\w.*')
+
 def functionRead(filePointer):
     fileString = filePointer.read()
-    print(fileString )
+    if reObj.search(fileString):
+        print("found something")
+    else:
+        print("Nothing i think so")
+
+    #print(fileString )
     del fileString
 
 filePointer = open("textFile.txt", 'r')
